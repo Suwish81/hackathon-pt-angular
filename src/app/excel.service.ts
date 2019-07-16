6ea0498c98f00a3faff9c3288f1159a69f5d0a10
Suwish81 <desktop.suwish@gmail.com>
2019-07-16 10:00:24 +0530

    3 files modified

--- src/app/excel.service.ts
+++ src/app/excel.service.ts
@@ -26,5 +26,38 @@ export class ExcelService {
     });
     FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
   }
+  
+  /*public getJSONFromExcel(ev) : any {
+    let workBook = null;
+    let jsonData = null;
+    const reader = new FileReader();
+    const file = ev.target.files[0];
+    
+    reader.onload = (event) => {
+      const data = reader.result;
+      workBook = XLSX.read(data, { type: 'binary' });
+      jsonData = workBook.SheetNames.reduce((initial, name) => {
+        const sheet = workBook.Sheets[name];
+        initial[name] = XLSX.utils.sheet_to_json(sheet);
+        return initial;
+      }, {});
+      dataString = JSON.stringify(jsonData);
+     // document.getElementById('output').innerHTML = dataString.slice(0, 300).concat("...");
+      //this.setDownload(dataString);
+    }
+    //reader.readAsBinaryString(file);
+    console.log(dataString);
+    return  JSON.parse(dataString);
+  }*/
+
+   setDownload(data) {
+    //this.willDownload = true;
+    setTimeout(() => {
+      const el = document.querySelector("#download");
+      el.setAttribute("href", `data:text/json;charset=utf-8,${encodeURIComponent(data)}`);
+      el.setAttribute("download", 'xlsxtojson.json');
+    }, 1000)
+  }
+
 
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -5,6 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { Item } from './item';
 import { ItemPrice } from './itemPrice';
 import {ExcelService} from './excel.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'hello',
@@ -32,6 +33,33 @@ export class HelloComponent implements OnInit {
   constructor(private http: HttpClient,private excelService:ExcelService) {
     this.home();
   }
+
+   //
+   onFileChange(env):any{
+     console.log('Uploading');
+    //this.selItemPriceList = this.excelService.getJSONFromExcel(env);
+
+    let workBook = null;
+    let jsonData = null;
+    const reader = new FileReader();
+    const file = env.target.files[0];
+    
+    reader.onload = (event) => {
+      const data = reader.result;
+      workBook = XLSX.read(data, { type: 'binary' });
+      jsonData = workBook.SheetNames.reduce((initial, name) => {
+        const sheet = workBook.Sheets[name];
+        initial[name] = XLSX.utils.sheet_to_json(sheet);
+        return initial;
+      }, {}); 
+      this.selItemPriceList=jsonData.data;
+      console.log( jsonData.data);
+     // document.getElementById('output').innerHTML = dataString.slice(0, 300).concat("...");
+      //this.setDownload(dataString);
+    }
+    reader.readAsBinaryString(file);
+
+   }
 
   downloadPriceList() :any{
      this.excelService.exportAsExcelFile(this.selItemPriceList, 'PriceList');
